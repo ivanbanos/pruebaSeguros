87c904f70dca87c2f14aa1b0109d327c7758dc9b
ivanalejandrobanos@gmail.com <ivanalejandrobanos@gmail.com>
2018-09-01 14:52:33 -0500

    First commit, initial project setup

---
+++ InsurranceSolution/InsurranceApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace InsurranceApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ InsurranceSolution/InsurranceApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace InsurranceApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
